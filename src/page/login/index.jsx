be6faeac39b062b30a8242edb30424dfc4f32658
Wahdaapf <wahdaadella.ba@gmail.com>
2024-02-24 19:31:54 +0700

    problem

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -7,37 +7,37 @@ const Login = () => {
     return (
         <div className="z-20 w-full h-full flex justify-center py-10">
             <div className="m-auto h-full overflow-auto w-[80vw] md:w-[70vw] lg:w-[70vw] flex flex-col items-center justify-start text-white p-4 pt-4 md:pt-10 lg:pt-10">
-                <p style={{ fontFamily: "Proxima Nova" }} className="text-4xl lg:text-4xl xl:text-xl text-center md:pt-10 md:p-0 md:pb-4 lg:pb-0 pb-0 font-thin">
+                <p style={{ fontFamily: "Proxima Nova" }} className="text-4xl lg:text-4xl xl:text-xl 2xl:text-3xl text-center md:pt-10 2xl:pt-20 md:p-0 md:pb-4 lg:pb-0 pb-0 font-thin">
                     WELKOM BIJ
                 </p>
-                <h1 style={{ fontFamily: "Proxima Nova" }} className="tabb text-4xl md:text-5xl xl:text-3xl font-bold text-center p-2 md:p-0 mb-4 ">
+                <h1 style={{ fontFamily: "Proxima Nova" }} className="tabb text-4xl md:text-5xl xl:text-3xl font-bold text-center p-2 md:p-0 mb-4 2xl:text-5xl ">
                     DE GEVAARLIJKE STOFFEN GAME
                 </h1>
                 <p style={{ fontFamily: "Proxima Nova" }} className="text-center font-thin text-xl md:text-2xl lg:text-xl">
                     Aan het einde van deze game, ben jij een nieuwe expert op het gebied van gevaarlijke stoffen. <br /> Om te starten, vul je naam en afdeling in.
                 </p>
                 <div>
-                    <div className="flex justify-end items-center mt-4 md:mt-12 lg:mt-4">
-                        <p style={{ fontFamily: "Proxima Nova" }} className="text-white mr-2 text-xl md:text-2xl lg:text-xl">
+                    <div className="flex justify-end items-center mt-4 md:mt-12 lg:mt-4 2xl:mt-8">
+                        <p style={{ fontFamily: "Proxima Nova" }} className="text-white mr-2 2xl:mr-8 text-xl md:text-2xl lg:text-xl 2xl:text-2xl">
                             Naam
                         </p>
-                        <div className="relative h-[6vh] md:h-[6vh] lg:h-[40px]">
-                            <img src={inputImage} alt="Gambar" className="w-40 md:w-80 h-full " />
+                        <div className="relative h-[6vh] md:h-[6vh] lg:h-[40px] 2xl:h-[7vh]">
+                            <img src={inputImage} alt="Gambar" className="w-40 md:w-80 2xl:w-[90%] h-full " />
                             <input type="text" className="absolute inset-1 w-36 md:w-80 h-auto text-white" />
                         </div>
                     </div>
                     <div className="flex justify-end items-center mt-4 md:mt-8 lg:mt-4">
-                        <p style={{ fontFamily: "Proxima Nova" }} className="text-white mr-2 text-xl md:text-2xl lg:text-xl">
+                        <p style={{ fontFamily: "Proxima Nova" }} className="text-white mr-2 2xl:mr-8 text-xl md:text-2xl lg:text-xl 2xl:text-2xl">
                             Afdeling
                         </p>
-                        <div className="relative h-[6vh] md:h-[6vh] lg:h-[40px]">
-                            <img src={inputImage} alt="Gambar" className="w-40 md:w-80 h-full " />
+                        <div className="relative h-[6vh] md:h-[6vh] lg:h-[40px] 2xl:h-[7vh]">
+                            <img src={inputImage} alt="Gambar" className="w-40 md:w-80 2xl:w-[90%] h-full " />
                             <input type="text" className="absolute inset-1 w-36 md:w-80 h-auto text-white" />
                         </div>
                     </div>
                 </div>
                 <button className="flex relative items-center justify-center h-auto w-[60%] md:w-[40%] lg:w-[30%] p-3 text-center mt-12 bg-no-repeat">
-                    <p className="text-white text-center text-xl">Login</p>
+                    <p className="text-white text-center text-xl 2xl:text-2xl">Login</p>
                     <div className="absolute w-full h-full">
                         <img src={buttonBg} alt="" className="w-full h-full" />
                     </div>
--- src/page/start/index.jsx
+++ src/page/start/index.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Main from "../../layout/index.jsx";
 import inputImage from "../../assets/base/button.png";
 import buttonBg from "../../assets/base/button.png";
+import { Link } from "react-router-dom";
 
 const Start = () => {
     return (
         <div className="z-20 w-full h-full flex justify-center py-10">
             <div className="m-auto h-full overflow-auto w-[80vw] md:w-[80vw] lg:w-[70vw] flex flex-col items-center justify-start text-white p-4 pt-4 md:pt-10 lg:pt-10">
-                <h1 style={{ fontFamily: "Proxima Nova" }} className="text-3xl md:text-6xl lg:text-3xl font-bold text-center p-2 md:p-10 lg:p-4">
+                <h1 style={{ fontFamily: "Proxima Nova" }} className="text-3xl md:text-6xl lg:text-3xl 2xl:text-5xl font-bold text-center p-2 md:p-10 lg:p-4 2xl:p-10">
                     BETA TEST
                 </h1>
-                <p style={{ fontFamily: "Proxima Nova" }} className="text-sm mt-4 md:mt-0 lg:mt-0 font-thin md:text-xl lg:text-xl">
+                <p style={{ fontFamily: "Proxima Nova" }} className="text-sm mt-4 md:mt-0 lg:mt-0 font-thin md:text-xl lg:text-xl 2xl:text-2xl">
                     Bedankt voor het meedoen aan de bètatest. Dit spel is een training over gevaarlijke stoffen, bedoeld om medewerkers van het IJsselland Ziekenhuis te leren hoe om te gaan met gevaarlijke stoffen. <br />
                     <br /> Het doel van deze test is om te observeren hoe het spel functioneert bij de medewerkers en om feedback te verzamelen over zowel het spel als de inhoud ervan. Om ons in staat te stellen mee te kijken tijdens het
                     spel, zouden we het zeer waarderen als je je scherm kunt opnemen tijdens het spelen. <br />
@@ -18,12 +19,12 @@ const Start = () => {
                     <br /> Bekijk de volgende video voor een korte uitleg over hoe je je scherm opneemt. <br />
                     <br /> Bedankt voor je deelname aan de test en veel plezier!
                 </p>
-                <button className="flex relative items-center justify-center h-auto w-[60%] md:w-[40%] lg:w-[30%] p-3 text-center mt-12 bg-no-repeat">
+                <Link to="/login" className="flex relative items-center justify-center h-auto w-[60%] md:w-[40%] lg:w-[30%] p-3 text-center mt-12 bg-no-repeat">
                     <p className="text-white text-center text-xl">Verder</p>
                     <div className="absolute w-full h-full">
                         <img src={buttonBg} alt="" className="w-full h-full" />
                     </div>
-                </button>
+                </Link>
             </div>
         </div>
     );
